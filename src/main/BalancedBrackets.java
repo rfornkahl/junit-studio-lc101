7f961fe76788abc879ded0fb5f74bbc28802eb45
Robert.Fornkahl <rwfornkahl@gmail.com>
2022-01-24 19:05:59 -0600

    completed studio 6

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -5,31 +5,43 @@ public class BalancedBrackets {
     /**
      * The function BalancedBrackets should return true if and only if
      * the input string has a set of "balanced" brackets.
-     *
+     * <p>
      * That is, whether it consists entirely of pairs of opening/closing
      * brackets (in that order), none of which mis-nest. We consider a bracket
      * to be square-brackets: [ or ].
-     *
+     * <p>
      * The string may contain non-bracket characters as well.
-     *
+     * <p>
      * These strings have balanced brackets:
-     *  "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
-     *
+     * "[LaunchCode]", "Launch[Code]", "[]LaunchCode", "", "[]"
+     * <p>
      * While these do not:
-     *   "[LaunchCode", "Launch]Code[", "[", "]["
+     * "[LaunchCode", "Launch]Code[", "[", "]["
      *
      * @param str - to be validated
      * @return true if balanced, false otherwise
      */
     public static boolean hasBalancedBrackets(String str) {
         int brackets = 0;
-        for (char ch : str.toCharArray()) {
-            if (ch == '[') {
-                brackets++;
-            } else if (ch == ']') {
-                brackets--;
+
+        if (str.indexOf('[', 0) < (str.indexOf(']', 1))){
+            for (char ch : str.toCharArray()) {
+                if (ch == '[') {
+                    brackets++;
+                } else if (ch == ']') {
+                    brackets--;
+                }
             }
+            return brackets == 0;
         }
-        return brackets == 0;
-    }
+        else
+        return false;
 }
+
+}
+
+
+
+
+
+
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,10 +1,16 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
+
+    //tests should guide how you revise the sourcecode. Use TDD to first write tests that will work for the
+    //desired behavior of BalancedBrackets. When you tests fail, correct he class to pass your tests.
+
+    //write at least 12 tests.
 
     //TODO: add tests here
     @Test
@@ -12,5 +18,91 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    //1
 
+    @Test
+    public void onlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void onlyStringNoBrakcets(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("chumbawamba"));
+    }
+
+    @Test
+    public void onlyBrakcetsWithSpaceReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[ ]"));
+    }
+
+    @Test
+    public void singleCharInBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[a]"));
+    }
+
+
+    @Test
+    public void singleLowerCaseStringInBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[abba]"));
+    }
+
+    @Test
+    public void singleUpperCaseStringInBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[ABBA]"));
+    }
+
+    @Test
+    public void multipleLowerCaseStringsInBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[chumbawamba tubthumping emi records]"));
+    }
+
+    @Test
+    public void closedBrackThenMutltipleStrings(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]chumbawamba tubthumping emi records"));
+    }
+
+    @Test
+    public void stringThenNewStringWithinClosedBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("chumbawamba tubthumping [emi] records"));
+    }
+
+    @Test
+    public void onlyOneBracketLeftWithStringReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[chumbawamba"));
+    }
+
+    @Test
+    public void twoTypesOfBracketsDontCloseWithStrings(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
+    @Test
+    public void twoTypesOfBracketsDontClose(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void onlyOneBracketRightWithMultipleStringsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("chumbawamba tubthumping]"));
+    }
+
+    @Test
+    public void onlyOneBracketLeftWithMultipleStringsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[chumbawamba tubthumping"));
+    }
+
+    @Test
+    public void onlyOneBracketRightWithStringReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("chumbawamba]"));
+    }
+
+    @Test
+    public void onlyOneBracketLeftReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void onlyOneBracketRightReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
 }
